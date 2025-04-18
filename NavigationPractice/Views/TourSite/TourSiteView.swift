f66c3e2aef00c6c5304a4f785923200758d72565
Randy Fong <randyfong@aol.com>
2025-04-18 09:19:02 -0700

    Add TourSiteView

---
+++ NavigationPractice/Views/TourSite/TourSiteView.swift
@@ -0,0 +1,23 @@
+//
+//  TourSiteView.swift
+//  NavigationPractice
+//
+//  Created by Randy Fong on 4/18/25.
+//
+
+import SwiftUI
+
+enum TourSite {
+    case overview
+    case conductTour
+}
+
+struct TourSiteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TourSiteView()
+}
